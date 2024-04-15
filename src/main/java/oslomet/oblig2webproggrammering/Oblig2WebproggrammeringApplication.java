eb16d1251fdc348634c68d6f3110da3afccb50c0
Emilieeiriksdottir <emilie.eiriksdottir@gmail.com>
2024-04-15 12:33:08 +0200

    Initial commit

---
+++ src/main/java/oslomet/oblig2webproggrammering/Oblig2WebproggrammeringApplication.java
@@ -0,0 +1,13 @@
+package oslomet.oblig2webproggrammering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2WebproggrammeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig2WebproggrammeringApplication.class, args);
+	}
+
+}
